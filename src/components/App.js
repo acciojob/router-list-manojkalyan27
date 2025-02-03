3be3e925f0aa7740cd3e2c735e4323073e0714b9
manojkalyan27 <147662541+manojkalyan27@users.noreply.github.com>
2025-02-03 14:44:10 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,19 @@
 
 import React from "react";
 import './../styles/App.css';
+import Homepage from './components/ritem.jsx';
+import { BrowserRouter as Router,Routes,Route } from "react-router-dom";
+import ItemList from './components/ItemList.jsx'
 
 const App = () => {
   return (
     <div>
-        {/* Do not remove the main div */}
+       <Router>
+                <Routes>
+                    <Route path='/' element={<Homepage />}/>
+                    <Route path='/items/:num' element={<ItemList />}/>
+                </Routes>
+            </Router>
     </div>
   )
 }
