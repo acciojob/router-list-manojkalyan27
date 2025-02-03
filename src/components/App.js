df6a85ebf4185e0828fba67a732041ca2eef4f6f
manojkalyan27 <147662541+manojkalyan27@users.noreply.github.com>
2025-02-03 14:55:00 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,11 @@
 
 import React from "react";
-import './../styles/App.css';
-import Homepage from '././components/ritem.jsx';
-import { BrowserRouter as Router,Routes,Route } from "react-router-dom";
-import ItemList from '././components/ItemList.jsx'
+// import './../styles/App.css';
+// import Homepage from '././components/ritem.jsx';
+import { BrowserRouter as Router,Routes,Route,Link } from "react-router-dom";
+// import ItemList from '././components/ItemList.jsx';
+import { useParams } from 'react-router-dom';
+
 
 const App = () => {
   return (
@@ -16,6 +18,33 @@ const App = () => {
             </Router>
     </div>
   )
+}
+
+const Homepage = () => {
+    return(
+        <>
+                <h3>Item List</h3>
+                <ul>
+                    <li><Link to='/items/1'>Item 1</Link></li>
+                    <li><Link to='/items/2'>Item 2</Link></li>
+                    <li><Link to='/items/3'>Item 3</Link></li>
+                </ul>
+        </>
+    )
+}
+
+const ItemList = () => {
+    const {num} = useParams();
+    
+    return(
+        <>
+            <div >
+                <h1>Item {num}</h1>
+                <p>Description for Item {num}</p>
+            </div>
+
+        </>
+    )
 }
 
 export default App
